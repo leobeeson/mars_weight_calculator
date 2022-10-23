a547b610c9464fe79232a81c00967302d9dcc65b
Francis Beeson <leobeeson@gmail.com>
2022-10-23 23:24:33 +0100

    Capture standard input into string variable.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    some_fn(&mut input);
+
     io::stdin().read_line(&mut input);
+    println!("Input: {}", input);
     let mars_weight: f32 = calculate_weight_on_mars(100.0);
     println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32{
     (weight/9.81) * 3.711
-}
-
-fn some_fn(s: &mut String) {
-    s.push_str("a");
 }
