3eda2fc0939a55a8e4db4f7d382271184938bed1
Francis Beeson <leobeeson@gmail.com>
2022-10-23 23:38:16 +0100

    Pass user input to weight calculator function.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg): ");
+    
     let mut input = String::new();
-
-    io::stdin().read_line(&mut input);
-    println!("Input: {}", input);
-    let mars_weight: f32 = calculate_weight_on_mars(100.0);
+    io::stdin().read_line(&mut input).unwrap();
+    
+    let weight: f32 = input.trim().parse().unwrap();
+    
+    let mars_weight: f32 = calculate_weight_on_mars(weight);
     println!("Weight on Mars: {}kg", mars_weight);
 }
 
